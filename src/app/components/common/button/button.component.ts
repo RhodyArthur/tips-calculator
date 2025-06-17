7d3c50f014442f79d9e311312ae946b0ba281228
RhodyArthur <rhodaarthur48@gmail.com>
2025-06-17 16:27:54 +0000

    add media queries

--- src/app/components/common/button/button.component.ts
+++ src/app/components/common/button/button.component.ts
@@ -9,14 +9,23 @@ import { Component, EventEmitter, Input, output, Output } from '@angular/core';
 })
 export class ButtonComponent {
   @Input() label: string = '';
-  @Input() color: string = '';
+  @Input() color: string = 'primary';
   @Input() textColor: string = '';
   @Input() value: number = 0;
+  @Input() isActive: boolean = false;
   
   tipSelected = output<number>();
 
 
   onTipSelected(value: number) {
     this.tipSelected.emit(value)
+  }
+
+  getButtonClasses(): { [key: string]: boolean } {
+    return {
+      'app-button': true,
+      [`button-${this.color}`]: true,
+      'button-active': this.isActive
+    };
   }
 }
